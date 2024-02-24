7bfb5076605feff09571dc02b281a2f609fceffe
raybac38 <hponsin@gmail.com>
2024-02-24 21:40:24 +0100

    network I/O

--- network.c
+++ network.c
@@ -1,11 +1,13 @@
 #include "network.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 Network * NetworkInitRandom(unsigned couche, unsigned nb_valeur)
 {
 
     Network * network = malloc(sizeof(Network));
     network->neurone = malloc(sizeof(SquareMatrix) * couche);
+    network->couche = couche;
 
     for (unsigned index = 0; index < couche; index++)
     {
@@ -15,6 +17,21 @@ Network * NetworkInitRandom(unsigned couche, unsigned nb_valeur)
     
 }
 
+Network * NetworkInit(unsigned couche, unsigned nb_valeur)
+{
+
+    Network * network = malloc(sizeof(Network));
+    network->neurone = malloc(sizeof(SquareMatrix) * couche);
+
+    network->couche = couche;
+
+    for (unsigned index = 0; index < couche; index++)
+    {
+        network->neurone[index] = SquareMatrixInit(nb_valeur);
+    }
+    return network;
+    
+}
 
 
 
@@ -24,6 +41,7 @@ Network * NetworkMutate(Network * network_ref, double mutation_power)
     unsigned couche = network_ref->couche;
     Network * network = malloc(sizeof(Network));
     network->neurone = malloc(sizeof(SquareMatrix) * couche);
+    network->couche = couche;
     for (unsigned index = 0; index < couche; index++)
     {
         network->neurone[index] = SquareMatrixMutate(network_ref->neurone[index], mutation_power);
@@ -49,3 +67,81 @@ void NetworkDestroy(Network ** network)
     return;
 }
 
+void NetworkSave(Network * network, char * filename)
+{
+    unsigned couche = network->couche;
+    unsigned taille = network->neurone[0]->taille;
+
+    FILE * f = fopen(filename, "wb");
+
+    fwrite(&couche, sizeof(unsigned), 1, f);
+    fwrite(&taille, sizeof(unsigned), 1, f);
+
+    for (unsigned index_couche = 0; index_couche < couche; index_couche++)
+    {
+        SquareMatrix * current_couche = network->neurone[index_couche];
+
+        for (unsigned index_colone = 0; index_colone < taille; index_colone++)
+        {
+            double * array_double = current_couche->valeurs[index_colone];
+
+            for (unsigned index_ligne = 0; index_ligne < taille; index_ligne++)
+            {
+                fwrite(&array_double[index_ligne], sizeof(double), 1, f);
+            }
+            
+        }
+        
+    }
+
+    NetworkDestroy(network);
+    fclose(f);
+}
+
+Network * NetworkLoad(char * filename)
+{
+    unsigned couche;
+    unsigned taille;
+
+    FILE * f = fopen(filename, "rb");
+
+    if(f == NULL)
+    {
+        printf("Unable to open file \n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(fread(&couche, sizeof(unsigned), 1, f) == 0)
+    {
+        printf("ERREUR lecture nombre de couche \n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(fread(&taille, sizeof(unsigned), 1, f) == 0)
+    {
+        printf("ERREUR lecture taille matrice\n");
+        exit(EXIT_FAILURE);
+    }
+    Network * network = NetworkInit(couche, taille);
+
+    for (unsigned index_couche = 0; index_couche < couche; index_couche++)
+    {
+        SquareMatrix * current_couche = network->neurone[index_couche];
+
+        for (unsigned index_colone = 0; index_colone < taille; index_colone++)
+        {
+            double * array_double = current_couche->valeurs[index_colone];
+
+            for (unsigned index_ligne = 0; index_ligne < taille; index_ligne++)
+            {
+                if(fread(&array_double[index_ligne], sizeof(double), 1, f) == 0)
+                {
+                    printf("ERREUR Lecture de donnee\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+    }
+    fclose(f);
+    return network;
+}
--- network.h
+++ network.h
@@ -13,3 +13,6 @@ Network * NetworkInitRandom(unsigned couche, unsigned nb_valeur);
 Network * NetworkMutate(Network * network_ref, double mutation_power);
 
 void NetworkDestroy(Network ** network);
+
+void NetworkSave(Network * network, char * filename);
+Network * NetworkLoad(char * filename);
